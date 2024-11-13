348f50a7262c14ea1951d3575bc7791d9340e05b
Alan Chen <alan@infinyon.com>
2024-11-13 14:50:00 -0800

    sdf: simplify startup

--- gaming-analytics/sdf/run.sh
+++ gaming-analytics/sdf/run.sh
@@ -1,9 +1,9 @@
-fluvio cluster start
-fluvio cluster status
-cd /workspace/connectors/ 
-cdk deploy start --ipkg infinyon-http-source-0.4.3.ipkg -c license-connector.yaml
-cdk deploy start --ipkg infinyon-http-source-0.4.3.ipkg -c car-connector.yaml
-cd /workspace
+# fluvio cluster start
+# fluvio cluster status
+# cd /workspace/connectors/
+# cdk deploy start --ipkg infinyon-http-source-0.4.3.ipkg -c license-connector.yaml
+# cdk deploy start --ipkg infinyon-http-source-0.4.3.ipkg -c car-connector.yaml
+# cd /workspace
 
 sdf clean
 initial_command="sdf run"
