f28b315efeb39b3f9d86db09b80ebb34f37ec67a
Alan Chen <alan@infinyon.com>
2024-11-14 12:20:08 -0800

    config: update to sdf-beta3

--- gaming-analytics/sdf/run.sh
+++ gaming-analytics/sdf/run.sh
@@ -6,19 +6,5 @@
 # cd /workspace
 
 sdf clean
-initial_command="sdf run"
-new_command="echo running"
 
-$initial_command > output.log 2>&1 &
-initial_pid=$!
-
-echo "Waiting for SDF to Start...(this will take a long time)"
-tail -f output.log | while read line; do
-    echo "$line"
-    if [[ "$line" == *"Welcome to SDF"* ]]; then
-        echo "Running SDF"
-        $new_command
-        pkill -P $$ tail
-        break
-    fi
-done
+sdf run
